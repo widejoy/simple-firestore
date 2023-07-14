35a84929f52b316da1a944abe35f1f5b5d16767b
widejoy <rogerantony40@ug.cusat.ac.in>
2023-07-14 13:24:59 +0530

    dynamic links and notifications work

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,61 +1,24 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:crud/models/basicmodel.dart';
-import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'home.dart';
 
-// ignore: camel_case_types
 class Homepage extends StatefulWidget {
-  const Homepage({super.key});
+  const Homepage({Key? key}) : super(key: key);
 
   @override
   State<Homepage> createState() => _HomepageState();
 }
 
+Future<void> handleBackgroundMessage(RemoteMessage message) async {
+  print("Title: ${message.notification?.title}");
+  print("Body: ${message.notification?.body}");
+  print("Payload: ${message.data}");
+}
+
 class _HomepageState extends State<Homepage> {
-  FirebaseDynamicLinks dynamicLinks = FirebaseDynamicLinks.instance;
-  String? _linkMessage;
-  bool _isCreatingLink = false;
-  final String DynamicLink = 'https://test-app/helloworld';
-  final String Link = 'https://reactnativefirebase.page.link/bFkn';
-
-  Future<void> initDynamicLinks() async {
-    dynamicLinks.onLink.listen((dynamicLinkData) {
-      Navigator.pushNamed(context, dynamicLinkData.link.path);
-    }).onError((error) {});
-  }
-
-  Future<void> _createDynamicLink(bool short) async {
-    setState(() {
-      _isCreatingLink = true;
-    });
-    final DynamicLinkParameters parameters = DynamicLinkParameters(
-      uriPrefix: 'https://cruddomain.page.link',
-      longDynamicLink: Uri.parse(
-        'https://reactnativefirebase.page.link/?efr=0&ibi=io.invertase.testing&apn=io.flutter.plugins.firebase.dynamiclinksexample&imv=0&amv=0&link=https%3A%2F%2Ftest-app%2Fhelloworld&ofl=https://ofl-example.com',
-      ),
-      link: Uri.parse(DynamicLink),
-      androidParameters: const AndroidParameters(
-        packageName: 'com.example.crud',
-        minimumVersion: 0,
-      ),
-    );
-
-    Uri url;
-    if (short) {
-      final ShortDynamicLink shortLink =
-          await dynamicLinks.buildShortLink(parameters);
-      url = shortLink.shortUrl;
-    } else {
-      url = await dynamicLinks.buildLink(parameters);
-    }
-
-    setState(() {
-      _linkMessage = url.toString();
-      _isCreatingLink = false;
-    });
-  }
+  final GlobalKey<NavigatorState> _navigatorKey = GlobalKey<NavigatorState>();
 
   void setupPushNotification() async {
     final fcm = FirebaseMessaging.instance;
@@ -66,24 +29,24 @@ class _HomepageState extends State<Homepage> {
       badge: true,
       sound: true,
     );
-    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-      String clickAction = message.data['clickAction'];
+    FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
 
-      if (clickAction == 'page_2') {
-        Navigator.push(
-          context,
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      String notificationId = message.data['notificationId'];
+
+      if (notificationId == 'page_2') {
+        _navigatorKey.currentState?.push(
           MaterialPageRoute(
             builder: (context) => const Home(),
           ),
         );
-      } else if (clickAction == 'page_1') {
-        Navigator.push(
-          context,
+      } else if (notificationId == 'page_1') {
+        _navigatorKey.currentState?.push(
           MaterialPageRoute(
             builder: (context) => const Homepage(),
           ),
         );
-      } else {}
+      }
     });
   }
 
@@ -99,73 +62,83 @@ class _HomepageState extends State<Homepage> {
       return FirebaseFirestore.instance.collection('data').snapshots().map(
             (event) => event.docs
                 .map(
-                  (e) => User.fromjson(
-                    e.data(),
-                  ),
+                  (e) => User.fromjson(e.data()),
                 )
                 .toList(),
           );
     }
 
-    return Scaffold(
-      body: StreamBuilder(
-        stream: readusers(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            final users = snapshot.data!;
+    return Navigator(
+      key: _navigatorKey,
+      onGenerateRoute: (settings) {
+        return MaterialPageRoute(
+          settings: settings,
+          builder: (context) {
+            return Scaffold(
+              body: StreamBuilder(
+                stream: readusers(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    final users = snapshot.data!;
 
-            return ListView(
-                children: users.map(
-              (e) {
-                return Dismissible(
-                  key: ValueKey(e.id),
-                  onDismissed: (direction) {
-                    final deleteuser =
-                        FirebaseFirestore.instance.collection('data').doc(e.id);
-                    deleteuser.delete();
-                  },
-                  child: ListTile(
-                    onTap: () {
-                      final updateuser = FirebaseFirestore.instance
-                          .collection('data')
-                          .doc(e.id);
-                      updateuser.delete();
-                      Navigator.push(context, MaterialPageRoute(
-                        builder: (context) {
-                          return const Home();
+                    return ListView(
+                      children: users.map(
+                        (e) {
+                          return Dismissible(
+                            key: ValueKey(e.id),
+                            onDismissed: (direction) {
+                              final deleteuser = FirebaseFirestore.instance
+                                  .collection('data')
+                                  .doc(e.id);
+                              deleteuser.delete();
+                            },
+                            child: ListTile(
+                              onTap: () {
+                                final updateuser = FirebaseFirestore.instance
+                                    .collection('data')
+                                    .doc(e.id);
+                                updateuser.delete();
+                                _navigatorKey.currentState?.push(
+                                  MaterialPageRoute(
+                                    builder: (context) => const Home(),
+                                  ),
+                                );
+                              },
+                              leading: CircleAvatar(
+                                child: Text(e.age.toString()),
+                              ),
+                              title: Text(e.name),
+                              subtitle: Text(e.birthday),
+                            ),
+                          );
                         },
-                      ));
+                      ).toList(),
+                    );
+                  } else if (snapshot.hasError) {
+                    return const Text('Something went wrong');
+                  } else {
+                    return const CircularProgressIndicator();
+                  }
+                },
+              ),
+              appBar: AppBar(
+                actions: [
+                  IconButton(
+                    onPressed: () {
+                      _navigatorKey.currentState?.push(
+                        MaterialPageRoute(
+                          builder: (context) => const Home(),
+                        ),
+                      );
                     },
-                    leading: CircleAvatar(
-                      child: Text(e.age.toString()),
-                    ),
-                    title: Text(e.name),
-                    subtitle: Text(e.birthday),
+                    icon: const Icon(Icons.add),
                   ),
-                );
-              },
-            ).toList());
-          } else if (snapshot.hasError) {
-            return const Text('something went wrong');
-          } else {
-            return const CircularProgressIndicator();
-          }
-        },
-      ),
-      appBar: AppBar(
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const Home(),
-                  ));
-            },
-            icon: const Icon(Icons.add),
-          ),
-        ],
-      ),
+                ],
+              ),
+            );
+          },
+        );
+      },
     );
   }
 }
